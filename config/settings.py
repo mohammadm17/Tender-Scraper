6b1c1f8a8572c0a126b203aafac5fde6fc54e5d3
Mohammad Ansari <76251182+mohammadm17@users.noreply.github.com>
2023-09-16 03:20:53 +0530

    Add files via upload

---
+++ config/settings.py
@@ -0,0 +1,11 @@
+# config.py
+
+# Webdriver path for Selenium (you can customize this)
+WEBDRIVER_PATH = '/path/to/geckodriver'
+
+# URLs for scraping
+WORLD_BANK_URL = "https://www.chinabidding.com/en"
+INTERNATIONAL_TENDERS_URL = "https://www.chinabidding.com/en"
+CPPC_ORG_URL = "https://www.cpppc.org/en/PPPyd.jhtml"
+INDIAN_TENDER_URL = 'https://etenders.gov.in/eprocure/app'
+CHINESE_TENDER_URL = 'https://www.chinabidding.com/en'
